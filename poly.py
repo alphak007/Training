e09952d613b82ef985eb6f5e4cebc32fad55a92c
Alok Shukla <56090810+alphak007@users.noreply.github.com>
2021-10-01 13:46:31 +0530

    polymorphism

---
+++ poly.py
@@ -0,0 +1,13 @@
+
+class Mango():
+	def price(self):
+		print("Mango is 130 per 1 kg")
+
+class Banana():
+	def price(self):
+		print("Banana is 120 per 1 kg")
+
+m=Mango()
+b=Banana()
+m.price()
+b.price()
